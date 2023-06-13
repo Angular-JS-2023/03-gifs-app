0a8a0f4f96c1ef2535736a6cf4d1837ca3d125b0
Luis Angel <luis.flores@durango.gob.mx>
2023-06-13 09:48:15 -0600

    Uso de ViewChild

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
@@ -7,14 +7,30 @@ import { Component, OnInit } from '@angular/core';
     <input 
       class="form-control" 
       type="text"
-      placeholder="Buscar gifs..">
+      placeholder="Buscar gifs.."
+      (keyup.enter)="searchTag()"
+      #txtTagInput>
+    <!-- <input 
+      class="form-control" 
+      type="text"
+      placeholder="Buscar gifs.."
+      (keyup.enter)="searchTag( txtTagInput.value )"
+      #txtTagInput> -->
   `
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent {
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
 
   constructor() { }
 
-  ngOnInit() {
+  // searchTag(newTag: string): void {
+  searchTag(): void {
+
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+
   }
 
 }
