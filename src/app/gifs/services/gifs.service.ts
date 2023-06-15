6b4aa90dd56a7b6b024c110c7d84963511cc8baa
Luis Angel <luis.flores@durango.gob.mx>
2023-06-15 10:45:18 -0600

    Añadiendo servicio de gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interface';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
+  public gifList: Gif[] = [];
+
+  private apiKey: string = 'ePlFW7Den6PN1B80z60l4jbYu66DzBYw';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
   private _tagsHistory: string[] = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   get tagsHistory() {
     // Quitar la referencia del arreglo principal
@@ -26,6 +35,28 @@ export class GifsService {
   searchTag(tag: string): void {
     if (tag.trim().length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params }).subscribe(rta => {
+     this.gifList = rta.data;    
+    });
+
   }
+
+  // async getResponse(): Promise<void> {
+  //   // #1 forma
+  //   // fetch('https://api.giphy.com/v1/gifs/search?api_key=ePlFW7Den6PN1B80z60l4jbYu66DzBYw&q=valorant&limit=10')
+  //   // .then( resp => resp.json())
+  //   // .then( data => console.log(data));
+
+  //   // #2 forma
+  //   // const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=ePlFW7Den6PN1B80z60l4jbYu66DzBYw&q=valorant&limit=10')
+  //   // const data = await resp.json();    
+  // }
   
 }
