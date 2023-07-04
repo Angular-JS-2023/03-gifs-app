5ea3026fd960dfa891bbdfd8f680963cb299a6ff
Luis Angel <luis.flores@durango.gob.mx>
2023-07-04 13:18:35 -0600

    Uso de localStorages

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -14,7 +14,9 @@ export class GifsService {
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     // Quitar la referencia del arreglo principal
@@ -30,6 +32,21 @@ export class GifsService {
 
     this._tagsHistory.unshift(tag); // insertar al inicio
     this._tagsHistory = this._tagsHistory.splice(0, 10); // Mantener 10 elementos en arreglo
+  
+    this.saveLocalStorage(); // Guardar informacion
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!); // ! siempre va a venir datos
+  
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   searchTag(tag: string): void {
