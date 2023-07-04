fa40fe39f13371ea613b6f7cf0fd6163adae765b
Luis Angel <luis.flores@durango.gob.mx>
2023-07-04 12:23:01 -0600

    Buscando gifs, mostrar gifs con sidebar

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interface';
 
 @Component({
   selector: 'gifs-card-list',
   templateUrl: './card-list.component.html'
 })
 export class CardListComponent {
+
+  @Input()
+  public gifs: Gif[] = [];
 
   constructor() { }
 
